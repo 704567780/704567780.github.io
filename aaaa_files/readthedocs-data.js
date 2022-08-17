f37bd1cd22d3e6b6f89c451d3a909d2979adc7cd
Yellow Duck <704567780@qq.com>
2022-08-17 19:15:44 +0800

    test

---
+++ aaaa_files/readthedocs-data.js
@@ -0,0 +1,19 @@
+var READTHEDOCS_DATA = {
+    project: "spinningupcn",
+    version: "latest",
+    language: "zh_CN",
+    programming_language: "py",
+    subprojects: {},
+    canonical_url: "https://spinningup.qiwihui.com/zh_CN/latest/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "a8dd22ce",
+    ad_free: false,
+
+    global_analytics_code: "UA-17997319-1",
+    user_analytics_code: "UA-46660488-7"
+};
+
