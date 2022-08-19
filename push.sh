7bbc1dae36d3e52207f3828420bc689ac71df158
Yellow Duck <704567780@qq.com>
2022-08-19 13:33:30 +0800

    push

--- push.sh
+++ push.sh
@@ -1,9 +1,17 @@
 python ./collect.py
+
 sleep 1s
 git add .
+
 sleep 1s
 git status
+
 sleep 1s
+echo "####### commit1 #######"
 git commit -m "push"
+echo "####### commit2 #######"
+
+echo "####### push1 #######"
 sleep 1s
 git push origin master
+echo "####### push2 #######"
